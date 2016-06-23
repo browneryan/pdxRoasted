c75cc66cf3f56fe94b776dfc96434524653c2cc8
browneryan <browneryan87@gmail.com>
2016-06-23 10:59:06 -0700

    Add validation service for email, required fields

---
+++ app/validation.service.ts
@@ -0,0 +1,17 @@
+export class ValidationService {
+    static getValidationErrorMessage(code: string) {
+        let config = {
+            'required': 'Required',
+            'invalidEmailAddress': 'Invalid email address'
+        };
+        return config[code];
+    }
+
+    static emailValidator(control) {
+        if (control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
+            return null;
+        } else {
+            return { 'invalidEmailAddress': true };
+        }
+    }
+}
