361d1c31cfe7bfdc0aa9f093af005b3ef44001bf
browneryan <browneryan87@gmail.com>
2016-06-23 10:48:17 -0700

    Add formBuilder constructor with links to Validators, also onSubmit function

--- app/contact.component.ts
+++ app/contact.component.ts
@@ -1,9 +1,28 @@
 import { Component } from 'angular2/core';
+import { FormBuilder, Validators } from 'angular2/common';
+import { ControlMessages } from './control-messages.component';
+import { ValidationService } from './validation.service';
 
 @Component({
   selector: 'contact',
+  directives: [ControlMessages],
   templateUrl: 'app/contact.component.html'
 })
 export class ContactComponent {
+  contactForm: any;
 
+  constructor(private _formBuilder: FormBuilder) {
+
+    this.contactForm = this._formBuilder.group({
+      'name': ['', Validators.required],
+      'email': ['', Validators.compose([Validators.required, ValidationService.emailValidator])],
+      'message': ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.dirty && this.contactForm.valid) {
+      alert(`Name: ${this.contactForm.value.name} Email: ${this.contactForm.value.email} Message: ${this.contactForm.value.message}`);
+    }
+  }
 }
